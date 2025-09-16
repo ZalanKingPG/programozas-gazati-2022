5db5fcac6a4fae7d4cb64816c7bfb8c45503d9dd
Zalan <elnezestmagaon00@gmail.com>
2025-09-16 19:34:32 +0200

    Delete 2022 directory

--- 2022/vasarlas.py
+++
@@ -1,8 +0,0 @@
-termek_ara = int(input("Kérem a termék árát: "))
-arfolyam = float(input("Kérem az euró árfolyamát: "))
-pez = float(input("Mennyi euróval rendelkezel: "))
-atvaltas = arfolyam * pez
-if termek_ara <= atvaltas:
-    print("A terméket meg tudod vásárolni")
-if termek_ara > atvaltas:
-    print("Nincs elég euród a termék megvásárlására!")
